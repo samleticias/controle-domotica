b444b61bc26dd45882e93673f9b17f65a00ce6a6
luanafleal <luanaferreirasilva913@gmail.com>
2025-09-03 11:04:21 -0300

    Adiciona frontend ao repositório principal

---
+++ domotica-frontend/assets/js/script.js
@@ -0,0 +1,6 @@
+// const sidebar = document.querySelector(".sidebar");
+// const sidebarToggle = document.querySelector(".sidebar-toggler");
+
+// sidebarToggle.addEventListener("click", () => {
+//     sidebar.classList.toggle("collapsed")
+// })
