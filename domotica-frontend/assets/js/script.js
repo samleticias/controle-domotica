4966acf9421ac661fc289d99238e9c7f515e419f
samleticias <sammyaleticia09@gmail.com>
2025-09-07 16:37:33 -0300

    remove pasta do frontend

--- domotica-frontend/assets/js/script.js
+++
@@ -1,6 +0,0 @@
-// const sidebar = document.querySelector(".sidebar");
-// const sidebarToggle = document.querySelector(".sidebar-toggler");
-
-// sidebarToggle.addEventListener("click", () => {
-//     sidebar.classList.toggle("collapsed")
-// })
